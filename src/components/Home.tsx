267f9ffa5e240862de955d1ebe56933e87338ff6
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-13 09:01:56 +0000

    Few style adjustments

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,16 @@
 function Home() {
   return (
     <div className="flex h-[100dvh] flex-col">
-      <header className="fixed flex w-[100dvw] justify-between px-6 py-2">
+      <header className="fixed flex w-[100dvw] justify-between px-2 py-2">
         <p>Home</p>
         <p>Contact</p>
       </header>
-      <div className="flex flex-1 flex-col justify-center leading-none">
-        <p className="text-[4vw]">Hi.</p>
-        <p className="ml-4 text-[6vw]">Im Clara.</p>
-        <p className="ml-8 text-[10vw]">Web Developer.</p>
+      <div className="flex flex-1 flex-col items-center justify-center leading-none">
+        <div>
+          <p className="text-[3vw]">Hi.</p>
+          <p className="-ml-[5%] text-[6vw]">Im Clara.</p>
+          <p className="text-[12vw]">Web Developer.</p>
+        </div>
       </div>
     </div>
   );

28407e1c5439c6293fd49d11c3eada77af948f6e
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-14 09:45:39 +0000

    Added smooth scrolling to Home content

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,27 @@
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+
 function Home() {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({ target: container, offset: ['start start', 'end center'] });
+  const transform = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+  const spring = useSpring(transform, { damping: 12, mass: 0.2, stiffness: 50 });
+
   return (
-    <div className="flex h-[100dvh] flex-col">
+    <motion.div ref={container} className="flex h-[100dvh] flex-col">
       <header className="fixed flex w-[100dvw] justify-between px-2 py-2">
         <p>Home</p>
         <p>Contact</p>
       </header>
       <div className="flex flex-1 flex-col items-center justify-center leading-none">
-        <div>
+        <motion.div style={{ y: spring, opacity }}>
           <p className="text-[3vw]">Hi.</p>
           <p className="-ml-[5%] text-[6vw]">Im Clara.</p>
           <p className="text-[12vw]">Web Developer.</p>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 

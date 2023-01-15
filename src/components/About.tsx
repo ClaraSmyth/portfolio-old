a6b977c7f948eb3031b72c9e25de8a03750a55dd
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-15 16:57:51 +0000

    Added smooth scroll to About and Projects sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,39 +11,50 @@ import {
   SiWebpack,
 } from 'react-icons/si';
 
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import { useSmoothScroll } from '../hooks';
+
 function About() {
-  const skills = [
-    { name: 'HTML', icon: <SiHtml5 size={'3em'} /> },
-    { name: 'CSS', icon: <SiCss3 size={'3em'} /> },
-    { name: 'JavaScript', icon: <SiJavascript size={'3em'} /> },
-    { name: 'SASS', icon: <SiSass size={'3em'} /> },
-    { name: 'React', icon: <SiReact size={'3em'} /> },
-    { name: 'TypeScript', icon: <SiTypescript size={'3em'} /> },
-    { name: 'Tailwind', icon: <SiTailwindcss size={'3em'} /> },
-    { name: 'Jest', icon: <SiJest size={'3em'} /> },
-    { name: 'Git', icon: <SiGit size={'3em'} /> },
-    { name: 'Webpack', icon: <SiWebpack size={'3em'} /> },
-  ];
+  const container = useRef(null);
+  const spring = useSmoothScroll(container);
 
   return (
-    <div className="grid h-[100dvh] grid-cols-2 items-center justify-items-center gap-4">
-      <div className="max-w-sm">
+    <motion.div
+      ref={container}
+      style={{ scrollSnapAlign: 'start' }}
+      className="grid h-[100dvh] grid-cols-2 items-center justify-items-center gap-4"
+    >
+      <motion.div style={{ y: spring }} className="max-w-sm">
         <p className="text-4xl font-bold">About Me</p>
         <p>
           I am a highly motivated self-taught web developer with a passion for creating responsive and user-friendly
           webpages. I am constantly learning and excited to continue honing my skills and growing as a developer.
         </p>
-      </div>
-      <div className="grid w-full max-w-md grid-cols-2 justify-items-end gap-4">
+      </motion.div>
+      <motion.div style={{ y: spring }} className="grid w-full max-w-md grid-cols-2 justify-items-end gap-4">
         {skills.map((item) => (
           <div key={item.name} className="flex items-center gap-2">
             <p className="text-lg font-medium">{item.name}</p>
             {item.icon}
           </div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
+
+const skills = [
+  { name: 'HTML', icon: <SiHtml5 size={'3em'} /> },
+  { name: 'CSS', icon: <SiCss3 size={'3em'} /> },
+  { name: 'JavaScript', icon: <SiJavascript size={'3em'} /> },
+  { name: 'SASS', icon: <SiSass size={'3em'} /> },
+  { name: 'React', icon: <SiReact size={'3em'} /> },
+  { name: 'TypeScript', icon: <SiTypescript size={'3em'} /> },
+  { name: 'Tailwind', icon: <SiTailwindcss size={'3em'} /> },
+  { name: 'Jest', icon: <SiJest size={'3em'} /> },
+  { name: 'Git', icon: <SiGit size={'3em'} /> },
+  { name: 'Webpack', icon: <SiWebpack size={'3em'} /> },
+];
 
 export default About;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,41 +1,48 @@
-function Projects() {
-  return (
-    <>
-      {projectArr.map((project) => (
-        <div key={project.title} className="grid h-[100dvh] grid-cols-2 items-center justify-items-center gap-4">
-          <div className="flex max-w-sm flex-col gap-4">
-            <h2 className="text-4xl font-bold">Project Title</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti placeat necessitatibus iusto a
-              commodi exercitationem culpa.
-            </p>
-            <div className="flex gap-2">
-              <a
-                className="rounded-lg bg-gray-300 py-2 px-4"
-                href="https://clarasmyth.github.io/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Demo
-              </a>
-              <a
-                className="py-2 px-4 underline underline-offset-4"
-                href="https://github.com/ClaraSmyth"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Code
-              </a>
-            </div>
-          </div>
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import { useSmoothScroll } from '../hooks';
 
-          <div className="flex flex-wrap gap-4">
-            <img className="aspect-auto w-24" src="https://via.placeholder.com/400x600" alt="" />
-            <img className="aspect-auto w-64" src="https://via.placeholder.com/600x400" alt="" />
-          </div>
+function Projects() {
+  const container = useRef(null);
+  const spring = useSmoothScroll(container);
+
+  return (
+    <motion.div
+      ref={container}
+      style={{ y: spring }}
+      className="grid h-[100dvh] grid-cols-2 items-center justify-items-center gap-4"
+    >
+      <div className="flex max-w-sm flex-col gap-4">
+        <h2 className="text-4xl font-bold">Project Title</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti placeat necessitatibus iusto a
+          commodi exercitationem culpa.
+        </p>
+        <div className="flex gap-2">
+          <a
+            className="rounded-lg bg-gray-300 py-2 px-4"
+            href="https://clarasmyth.github.io/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+          <a
+            className="py-2 px-4 underline underline-offset-4"
+            href="https://github.com/ClaraSmyth"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Code
+          </a>
         </div>
-      ))}
-    </>
+      </div>
+
+      <div className="flex flex-wrap gap-4">
+        <img className="aspect-auto w-24" src="https://via.placeholder.com/400x600" alt="" />
+        <img className="aspect-auto w-64" src="https://via.placeholder.com/600x400" alt="" />
+      </div>
+    </motion.div>
   );
 }
 

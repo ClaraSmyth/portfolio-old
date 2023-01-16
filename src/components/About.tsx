883c3fd79519314774ff444ab8849434cbd66f2d
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-16 15:58:47 +0000

    Updated font sizes and layout for different screen

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -24,20 +24,23 @@ function About() {
       ref={container}
       className="grid h-[100dvh] grid-cols-1 items-center justify-items-center gap-4 md:grid-cols-2"
     >
-      <motion.div style={{ y: spring }} className="max-w-sm p-2 text-center md:text-start">
-        <p className="text-4xl font-bold">About Me</p>
-        <p>
+      <motion.div
+        style={{ y: spring }}
+        className="flex max-w-sm flex-col gap-4 p-2 text-center md:text-start 2xl:max-w-lg"
+      >
+        <p className="text-4xl font-bold 2xl:text-5xl">About Me</p>
+        <p className="2xl:text-xl">
           I am a highly motivated self-taught web developer with a passion for creating responsive and user-friendly
           webpages. I am constantly learning and excited to continue honing my skills and growing as a developer.
         </p>
       </motion.div>
       <motion.div
         style={{ y: spring }}
-        className="grid w-full max-w-md grid-cols-[max-content,1fr] justify-items-end gap-4 p-2"
+        className="grid w-full max-w-sm grid-cols-[max-content,1fr] justify-items-end gap-4 p-2 2xl:max-w-xl 2xl:gap-8 2xl:text-xl"
       >
         {skills.map((item) => (
           <div key={item.name} className="flex items-center gap-2">
-            <p className="text-lg font-medium">{item.name}</p>
+            <p className="text-lg font-medium 2xl:text-2xl">{item.name}</p>
             {item.icon}
           </div>
         ))}

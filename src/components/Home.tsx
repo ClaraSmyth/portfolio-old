8082d8d36e9b99108cf679fcbf3cb20b5730220a
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-17 13:16:02 +0000

    Converted Home text into a canvas element

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,41 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useSmoothScroll } from '../hooks';
 import { MdOutlineMailOutline } from 'react-icons/md';
 
 function Home() {
-  const container = useRef(null);
+  const container = useRef<HTMLDivElement | null>(null);
   const spring = useSmoothScroll(container);
+
+  useEffect(() => {
+    const canvas = document.querySelector('canvas');
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    // Need to set canvas width and height here to avoid distortions
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Font Styling
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'center';
+
+    const textArr = ['Clara', 'Smyth', 'Web Developer'];
+    const textX = canvas.width / 2;
+    const textY = canvas.height / 2 - 100;
+
+    textArr.forEach((word, index) => {
+      if (word === 'Web Developer') {
+        ctx.font = 'max(2rem,3vw) Lusitana';
+        ctx.fillText(word, textX, textY + index * 160);
+        return;
+      }
+      ctx.font = 'bold 200px Raleway';
+      ctx.fillText(word, textX, textY + index * 200);
+    });
+  }, []);
 
   return (
     <div ref={container} className="flex h-[100dvh] flex-col">
@@ -16,9 +46,10 @@ function Home() {
       </header>
       <div className="flex flex-1 flex-col items-center justify-center text-center leading-none">
         <motion.div style={{ y: spring }}>
-          <h2 className="text-[max(6rem,15vw)] leading-none">Clara</h2>
+          <canvas className="canvas bg-slate-400"></canvas>
+          {/* <h2 className="text-[max(6rem,15vw)] leading-none">Clara</h2>
           <h2 className="mb-24 text-[max(6rem,15vw)] leading-none">Smyth</h2>
-          <p className="text-[max(2rem,3vw)] leading-none">Web Developer</p>
+          <p className="text-[max(2rem,3vw)] leading-none">Web Developer</p> */}
         </motion.div>
       </div>
     </div>

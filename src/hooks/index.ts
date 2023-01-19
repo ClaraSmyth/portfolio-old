b77b9a8744288760a7bf8b7549b1c3dad0bcebd0
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-19 14:02:32 +0000

    Created custom hook for cycling through projects

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useSmoothScroll } from './useSmoothScroll';
 export { default as useSmoothScrollRotate } from './useSmoothScrollRotate';
 export { default as useCanvasEffect } from './useCanvasEffect';
+export { default as useProjectsArr } from './useProjectsArr';

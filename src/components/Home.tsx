ede8ce359f0994c05183c0ab3f5c0bacbe592e30
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-19 16:19:34 +0000

    Removed old code + fixed icon size

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,16 +11,13 @@ function Home() {
   return (
     <div ref={container} className="flex h-[100svh] flex-col">
       <header className="fixed z-50 flex w-full justify-end p-2">
-        <a className="fold relative" href="mailto:clarasmyth.work@gmail.com">
+        <a className="fold relative text-base" href="mailto:clarasmyth.work@gmail.com">
           <MdOutlineMailOutline size={'2em'} />
         </a>
       </header>
       <div className="flex flex-1 flex-col items-center justify-center text-center leading-none">
         <motion.div style={{ y: spring }}>
           <canvas className="canvas bg-white"></canvas>
-          {/* <h2 className="text-[max(6rem,15vw)] leading-none">Clara</h2>
-          <h2 className="mb-24 text-[max(6rem,15vw)] leading-none">Smyth</h2>
-          <p className="text-[max(2rem,3vw)] leading-none">Web Developer</p> */}
         </motion.div>
       </div>
     </div>

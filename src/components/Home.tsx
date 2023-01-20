7e96bdd889040d953d3ea1decc3ebbe6b3962629
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-20 00:12:36 +0000

    Added a call to action button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import { motion } from 'framer-motion';
 import { useSmoothScroll, useCanvasEffect } from '../hooks';
-import { MdOutlineMailOutline } from 'react-icons/md';
+import { MdOutlineMailOutline, MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 function Home() {
   const container = useRef<HTMLDivElement | null>(null);
@@ -9,7 +9,7 @@ function Home() {
   useCanvasEffect();
 
   return (
-    <div ref={container} className="flex h-[100svh] flex-col">
+    <div id="home" ref={container} className="grid h-[100svh] grid-rows-[1fr]">
       <header className="fixed z-50 flex w-full justify-end p-2">
         <a className="fold relative text-base" href="mailto:clarasmyth.work@gmail.com">
           <MdOutlineMailOutline size={'2em'} />
@@ -20,6 +20,13 @@ function Home() {
           <canvas className="canvas bg-white"></canvas>
         </motion.div>
       </div>
+      <motion.button
+        style={{ y: spring }}
+        onClick={() => document.getElementById('projects')?.scrollIntoView()}
+        className="absolute bottom-0 animate-bounce justify-self-center rounded-full transition duration-300 hover:bg-gray-300"
+      >
+        <MdOutlineKeyboardArrowDown size={'3em'} className="" />
+      </motion.button>
     </div>
   );
 }

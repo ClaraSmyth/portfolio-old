03463a4f9cf7aba749ecf3249628333b7419ffe6
Clara Smyth <clarasmyth.work@gmail.com>
2023-01-21 22:53:28 +0000

    Added background svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { Home, Projects, About } from './components';
+import background from './assets/stacked-waves-haikei.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      style={{
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${background})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+      className="App"
+    >
       <Home />
       <Projects />
       <About />
